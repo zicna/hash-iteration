d04d58fe9628a1e9752429f70989787b7813fb7f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2020-12-11 23:11:08 -0600

    commit tree 2b174c0304b3cccafd414bb7b17b29aa4347f5b0

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,4 +8,12 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
+  passengers.each do |key, value|
+    #:key == key:
+    if key == :suite_a && value.start_with?("A")
+      winner =  passengers[key]
+    end
+  end
+  winner
 end
